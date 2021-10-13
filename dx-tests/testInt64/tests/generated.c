8f04bbd5399851e70854c6509c367efde2249e57
syuan <shenghao.yuan@inria.fr>
2021-10-13 13:03:11 +0200

    delete the dx-tests folder

--- dx-tests/testInt64/tests/generated.c
+++
@@ -1,8 +0,0 @@
-extern unsigned long long testincr(unsigned long long);
-
-unsigned long long testincr(unsigned long long a)
-{
-  return a + 1LLU;
-}
-
-
--- dx-tests/testListGet/tests/generated.c
+++
@@ -1,8 +0,0 @@
-extern unsigned long long testget(unsigned int, unsigned long long, unsigned long long, unsigned long long *);
-
-unsigned long long testget(unsigned int fuel, unsigned long long init, unsigned long long idx, unsigned long long *l)
-{
-  return *(l + idx);
-}
-
-
