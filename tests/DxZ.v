f30e3e8d3e01f929e12ae408919a165bba65baef
nuaaysh <shyuan@nuaa.edu.cn>
2021-10-20 23:03:31 +0200

    adding more constants: z_8, ... z_0xffff

--- tests/DxZ.v
+++ tests/DxZ.v
@@ -25,6 +25,12 @@ Definition z_0x84 := 0x84.
 Definition z_0x95 := 0x95.
 
 Definition z_0xff := 0xff.
+Definition z_8    := 8.
+Definition z_12   := 12.
+Definition z_32   := 32.
+Definition z_48   := 48.
+Definition z_0xfff := 0xfff.
+Definition z_0xffff := 0xffff.
 
 Definition C_Z_0x07: Csyntax.expr :=
   Csyntax.Eval (Vlong (Int64.repr z_0x07)) C_S64.
@@ -43,6 +49,18 @@ Definition C_Z_0x95: Csyntax.expr :=
 
 Definition C_Z_0xff: Csyntax.expr :=
   Csyntax.Eval (Vlong (Int64.repr z_0xff)) C_S64.
+Definition C_Z_8:  Csyntax.expr :=
+  Csyntax.Eval (Vlong (Int64.repr z_8)) C_S64.
+Definition C_Z_12: Csyntax.expr :=
+  Csyntax.Eval (Vlong (Int64.repr z_12)) C_S64.
+Definition C_Z_32: Csyntax.expr :=
+  Csyntax.Eval (Vlong (Int64.repr z_32)) C_S64.
+Definition C_Z_48: Csyntax.expr :=
+  Csyntax.Eval (Vlong (Int64.repr z_48)) C_S64.
+Definition C_Z_0xfff: Csyntax.expr :=
+  Csyntax.Eval (Vlong (Int64.repr z_0xfff)) C_S64.
+Definition C_Z_0xffff: Csyntax.expr :=
+  Csyntax.Eval (Vlong (Int64.repr z_0xffff)) C_S64.
 
 Definition zSymbolType :=
   MkCompilableSymbolType nil (Some ZCompilableType).
@@ -56,6 +74,13 @@ Definition Const_Z_0x84 := constant zSymbolType z_0x84 C_Z_0x84.
 Definition Const_Z_0x95 := constant zSymbolType z_0x95 C_Z_0x95.
 Definition Const_Z_0xff := constant zSymbolType z_0xff C_Z_0xff.
 
+Definition Const_Z_8  := constant zSymbolType z_8  C_Z_8.
+Definition Const_Z_12 := constant zSymbolType z_12 C_Z_12.
+Definition Const_Z_32 := constant zSymbolType z_32 C_Z_32.
+Definition Const_Z_48 := constant zSymbolType z_48 C_Z_48.
+Definition Const_Z_0xfff := constant zSymbolType z_0xfff C_Z_0xfff.
+Definition Const_Z_0xffff := constant zSymbolType z_0xffff C_Z_0xffff.
+
 Close Scope Z_scope.
 
 Module Exports.
@@ -67,5 +92,12 @@ Module Exports.
   Definition Const_Z_0x0c := Const_Z_0x0c.
   Definition Const_Z_0x84 := Const_Z_0x84.
   Definition Const_Z_0x95 := Const_Z_0x95.
+
   Definition Const_Z_0xff := Const_Z_0xff.
+  Definition Const_Z_8    := Const_Z_8.
+  Definition Const_Z_12   := Const_Z_12.
+  Definition Const_Z_32   := Const_Z_32.
+  Definition Const_Z_48   := Const_Z_48.
+  Definition Const_Z_0xfff := Const_Z_0xfff.
+  Definition Const_Z_0xffff := Const_Z_0xffff.
 End Exports.
