2093722dad694265ad3448ba8eaef58a10d4381d
syuan <shenghao.yuan@inria.fr>
2021-10-21 16:43:23 +0200

    moving all C_op expressions to this file and adding other C_bin_op

--- tests/CoqIntegers.v
+++ tests/CoqIntegers.v
@@ -32,3 +32,53 @@ Definition C_S16_pointer := Ctypes.Tpointer C_S16 Ctypes.noattr.
 Definition C_S32_pointer := Ctypes.Tpointer C_S32 Ctypes.noattr.
 
 Definition C_S64_pointer := Ctypes.Tpointer C_S64 Ctypes.noattr.
+
+(******************** C_U64 operations *******************)
+
+Definition C_U64_neg (x: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Eunop Cop.Oneg x C_U64.
+
+Definition C_U64_add (x y: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Ebinop Cop.Oadd x y C_U64.
+
+Definition C_U64_sub (x y: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Ebinop Cop.Osub x y C_U64.
+
+Definition C_U64_mul (x y: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Ebinop Cop.Omul x y C_U64.
+
+Definition C_U64_div (x y: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Ebinop Cop.Odiv x y C_U64.
+
+Definition C_U64_mod (x y: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Ebinop Cop.Omod x y C_U64.
+
+Definition C_U64_and (x y: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Ebinop Cop.Oand x y C_U64.
+
+Definition C_U64_or (x y: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Ebinop Cop.Oor x y C_U64.
+
+Definition C_U64_shl (x y: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Ebinop Cop.Oshl x y C_U64.
+
+Definition C_U64_shr (x y: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Ebinop Cop.Oshr x y C_U64.
+
+Definition C_U64_eq (x y: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Ebinop Cop.Oeq x y C_U64.
+
+Definition C_U64_ne (x y: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Ebinop Cop.One x y C_U64.
+
+Definition C_U64_lt (x y: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Ebinop Cop.Olt x y C_U64.
+
+Definition C_U64_gt (x y: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Ebinop Cop.Ogt x y C_U64.
+
+Definition C_U64_le (x y: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Ebinop Cop.Ole x y C_U64.
+
+Definition C_U64_ge (x y: Csyntax.expr) : Csyntax.expr :=
+  Csyntax.Ebinop Cop.Oge x y C_U64.
