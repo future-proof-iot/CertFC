6b4991211d18d4fb241682644e97325d17d2c03a
nuaaysh <shyuan@nuaa.edu.cn>
2021-10-23 19:45:30 +0200

    modify the state of monad and add monad state operations: upd/eval reg and pc

--- tests/DxMonad.v
+++ tests/DxMonad.v
@@ -1,11 +1,11 @@
 Require Import DxIntegers DxValues DxRegs.
 
-Definition state := regmap. (*This one must be int_64 defined in DxIntegers *)
+Definition stateM := state. (*This one must be int_64 defined in DxIntegers *)
 
 
-Definition M (A: Type) := state -> option (A * state).
+Definition M (A: Type) := stateM -> option (A * state).
 
-Definition runM {A: Type} (x: M A) (st: state) := x st.
+Definition runM {A: Type} (x: M A) (st: stateM) := x st.
 Definition returnM {A: Type} (a: A) : M A := fun st => Some (a, st).
 Definition emptyM {A: Type} : M A := fun st => None.
 Definition bindM {A B: Type} (x: M A) (f: A -> M B) : M B :=
@@ -15,8 +15,10 @@ Definition bindM {A B: Type} (x: M A) (f: A -> M B) : M B :=
     | Some (x', st') => runM (f x') st'
     end.
 
-Definition eval_reg (r: reg) : M val64_t := fun st => Some (eval_regmap r st, st).
-Definition upd_reg (r: reg) (v: val64_t) : M unit := fun st => Some (tt, upd_regmap r v st).
+Definition eval_pc: M int64_t := fun st => Some (eval_pc_tot st, st).
+Definition upd_pc (p: int64_t): M unit := fun st => Some (tt, upd_pc_tot p st).
+Definition eval_reg (r: reg) : M val64_t := fun st => Some (eval_reg_tot r st, st).
+Definition upd_reg (r: reg) (v: val64_t) : M unit := fun st => Some (tt, upd_reg_tot r v st).
 
 Declare Scope monad_scope.
 Notation "'do' x <- a ; b" :=
