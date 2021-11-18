efe19b578f9514c5d97190d257eaaee6544b3414
syuan <shenghao.yuan@inria.fr>
2021-11-18 16:43:50 +0100

    split the correct lemma as two parts: the first part is stuck QED

--- codeTest/test_check_mem_aux.v
+++ codeTest/test_check_mem_aux.v
@@ -82,6 +82,91 @@ Proof.
   intros; destruct (fst c) in H; rewrite H; eexists; reflexivity.
 Qed.
 
+Lemma correct_function_check_mem_aux_correct_aux:
+  forall
+(c : (fun x : CompilableType => coqType x * val) mem_regionCompilableType)
+(c0 : (fun x : CompilableType => coqType x * val) val64CompilableType)
+(c1 : (fun x : CompilableType => coqType x * val) memoryChunkCompilableType)
+(k : cont)
+(st : stateM)
+(m : mem)
+(Hsndc : snd c = block_ptr (fst c))
+(Hptrc : Vlong Int64.one = block_ptr (fst c))
+(b : block)
+(ofs : ptrofs)
+(vaddr vsize : int64)
+(Hptr : snd c = Vptr b ofs)
+(Hmaddr : Mem.loadv AST.Mint64 m (Vptr b ofs) = Some (start_addr (fst c)))
+(Haddr : Vlong vaddr = start_addr (fst c))
+(Hmsize : Mem.loadv AST.Mint64 m (Vptr b (Ptrofs.add ofs (Ptrofs.repr 8))) =
+         Some (block_size (fst c)))
+(Hsize : Vlong vsize = block_size (fst c))
+(Heq_c0 : fst c0 = snd c0)
+(v' : int64)
+(Hc0_vlong : Vlong v' = snd c0)
+(Hfst_c1 : match fst c1 with
+          | AST.Mint8unsigned => snd c1 = Vint Int.one
+          | AST.Mint16unsigned => snd c1 = Vint (Int.repr 2)
+          | AST.Mint32 => snd c1 = Vint (Int.repr 4)
+          | AST.Mint64 => snd c1 = Vint (Int.repr 8)
+          | _ => snd c1 = Vint (Int.repr 0)
+          end)
+(H0 : match_mem st (globalenv p) m)
+(x : int)
+(Hchunk : snd c1 = Vint x)
+(c2 : coqType'
+       (compilableSymbolResType
+          {| compilableSymbolArgTypes := Args; compilableSymbolResType := Some Res |}))
+(s : DxState.state)
+(Hf : f (fst c) (fst c0) (fst c1) st = Some (c2, s)),
+  (*exists t2,*)
+Smallstep.plus (fun ge : genv => step ge (function_entry2 ge))
+  (globalenv p)
+  (State fn (fn_body fn) k empty_env
+     (Maps.PTree.set _chunk1 (snd c1)
+        (Maps.PTree.set _addr0 (snd c0)
+           (Maps.PTree.set _mr3 (snd c) (create_undef_temps (fn_temps fn))))) m) 
+  Events.E0 (Returnstate c2 (call_cont k) m).
+Proof.
+  intros.
+  (*exists Events.E0.*)
+  eapply Smallstep.plus_left' with (t1:= Events.E0) (t2:= Events.E0); eauto.
+  repeat econstructor; eauto. (** call is_well_chunk_bool *)
+  eapply Smallstep.plus_left' with (t1:= Events.E0) (t2:= Events.E0); eauto.
+  repeat econstructor; eauto.
+  eapply Smallstep.plus_left' with (t1:= Events.E0) (t2:= Events.E0); eauto.
+  econstructor; eauto.
+  simpl; reflexivity.
+  econstructor.
+  eapply eval_Evar_global. (**r we must tell coq to evaluate the goal with this inductive constructor otherwise coq will use another one *)
+  reflexivity.
+  reflexivity.
+  simpl.
+  eapply deref_loc_reference. (**r coq doesn't know which constructor is correct *)
+  simpl; reflexivity.
+  econstructor; eauto.
+  econstructor; eauto.
+  reflexivity.
+  simpl.
+  unfold coqType, memoryChunkCompilableType in Hchunk.
+  (* Set Printing All. *)
+  rewrite Hchunk.
+  reflexivity.
+  econstructor; eauto.
+  econstructor; eauto.
+  reflexivity.
+  unfold f, check_mem_aux in Hf.
+  match goal with
+  | W : @bindM _ _ ?F1 ?F2 _ = _ |- _ =>
+      unfold bindM at 1 in W;
+      unfold runM in W;
+      let X := fresh "F1" in
+      let v := fresh "r" in
+      let st' := fresh "st" in
+      destruct F1 as [(v,st')|] eqn:X ; try congruence
+  end.
+Qed.
+
 Lemma correct_function_check_mem_aux_correct : correct_function p Args Res f fn match_mem match_arg_list match_res.
 Proof.
   constructor.
@@ -100,7 +185,8 @@ Proof.
 
   destruct (f _ _ _ _) eqn: Hf.
   destruct p0.
-  do 3 eexists.
+  eexists. exists m, Events.E0.
+  (*do 3 eexists.*)
   repeat split; unfold step2.
   apply Smallstep.plus_star;
   eapply Smallstep.plus_left'; eauto.
@@ -110,7 +196,7 @@ Proof.
   + simpl; unfold Coqlib.list_disjoint; simpl; intuition (subst; discriminate).
   + repeat econstructor; eauto.
   + reflexivity.
-  + eapply Smallstep.plus_left'; eauto.
+  + apply correct_function_check_mem_aux_correct_aux with (b:=b) (st := st) (ofs:=ofs) (vaddr:=vaddr) (vsize:=vsize)(v':=v') (x:=x) (s:=s); assumption. (*eapply Smallstep.plus_left'; eauto.
     repeat econstructor; eauto. (** call is_well_chunk_bool *)
     eapply Smallstep.plus_left'; eauto.
     repeat econstructor; eauto.
@@ -142,14 +228,15 @@ Proof.
         let v := fresh "r" in
         let st' := fresh "st" in
         destruct F1 as [(v,st')|] eqn:X ; try congruence
-    end.
+    end.*)
+  + repeat (rewrite Events.E0_left). eauto.
   + unfold match_mem in H0; destruct H0.
     assert (Heq_st: st = s). {
       unfold f, check_mem_aux, is_well_chunk_bool, bindM, returnM, runM in Hf.
       unfold coqType', compilableSymbolResType, coqType, Res, val64CompilableType, val64_t, stateM in Hf.
       destruct (@fst AST.memory_chunk val c1) eqn: Hfst_eq in Hf; congruence.
       }
-      rewrite Heq_st in y; eapply y.
+      rewrite Heq_st in H0; assumption.
   + (**res *)
     simpl in c2.
     unfold f, check_mem_aux, is_well_chunk_bool, bindM, runM, returnM in Hf.
@@ -158,8 +245,6 @@ Proof.
   + unfold f, check_mem_aux, is_well_chunk_bool, bindM, runM, returnM in Hf.
     unfold coqType', compilableSymbolResType, coqType, Res, val64CompilableType, val64_t, stateM in Hf.
     destruct (@fst AST.memory_chunk val c1) eqn: Hfst_eq in Hf; congruence.
-  Unshelve.
-  apply Events.E0.
 Qed.
 
 Definition ft := 0%nat.
