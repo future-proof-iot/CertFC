bf36e877cf8ce39e2402d5a9021bea018367522b
syuan <shenghao.yuan@inria.fr>
2021-12-01 15:08:24 +0100

    mark: need to declare the monad state somewhere

--- codeTest/correct_upd_pc.v
+++ codeTest/correct_upd_pc.v
@@ -12,7 +12,80 @@ static void upd_pc(struct bpf_state* st, unsigned long long pc) {
   return ;
 }
 Definition upd_pc (p: int64_t): M unit := fun st => Some (tt, upd_pc p st).
-*)
+ *)
+
+(*
+Ltac forward_seq :=
+  match goal with
+  | |- Smallstep.plus _ _ (State _ (Ssequence _ _) _ _ _ _) _ _ =>
+      eapply Smallstep.plus_left'; eauto; [eapply step_seq | idtac]
+  end.
+
+Ltac forward_call_one_arg :=
+  match goal with
+  | |- Smallstep.plus _ _ (State _ (Scall _ _ _) _ _ _ _) _ _ =>
+      eapply Smallstep.plus_left'; eauto; [eapply step_call; [reflexivity | eapply eval_Elvalue;[eapply eval_Evar_global; reflexivity | eapply deref_loc_reference; reflexivity] | eapply eval_Econs; [eapply eval_Etempvar; reflexivity | reflexivity | eapply eval_Enil] | econstructor; eauto | reflexivity] | idtac]
+  end.
+
+Ltac forward_returnstate :=
+  match goal with
+  | |- Smallstep.plus _ _ (Returnstate _ _ _) _ _ =>
+      eapply Smallstep.plus_left'; eauto; [eapply step_returnstate | idtac]
+  end.
+
+Ltac forward_skip_seq :=
+  match goal with
+  | |- Smallstep.plus _ _ (State _ Sskip (Kseq _ _) _ _ _) _ _ =>
+      eapply Smallstep.plus_left'; eauto; [eapply step_skip_seq | idtac]
+  end.
+
+Ltac forward_if :=
+  match goal with
+  | |- Smallstep.plus _ _ (State _ (Sifthenelse _ _ _) _ _ _ _) _ _ =>
+  eapply Smallstep.plus_left'; eauto; [eapply step_ifthenelse; [eapply eval_Etempvar; rewrite Maps.PTree.gss; reflexivity | reflexivity] | idtac]
+  end.*)
+
+Ltac forward_plus :=
+ match goal with
+  (** forward_seq *)
+  | |- Smallstep.plus _ _ (State _ (Ssequence _ _) _ _ _ _) _ _ =>
+      eapply Smallstep.plus_left'; eauto; [eapply step_seq | idtac]
+  (** forward_call_one_arg *)
+  | |- Smallstep.plus _ _ (State _ (Scall _ _ _) _ _ _ _) _ _ =>
+      eapply Smallstep.plus_left'; eauto;
+      [eapply step_call;
+        [reflexivity |                                (** goal: classify_fun *)
+         eapply eval_Elvalue;                         (** goal: eval_expr *)
+          [eapply eval_Evar_global; reflexivity |     (** eval_lvalue *)
+           eapply deref_loc_reference; reflexivity] | (** goal: deref_loc *)
+         eapply eval_Econs;                           (** goal: eval_exprlist *)
+          [eapply eval_Etempvar; reflexivity |        (** goal: eval_expr *)
+           reflexivity |                              (** goal: Cop.sem_cast *)
+           eapply eval_Enil] |                        (** goal: eval_exprlist *)
+         econstructor; eauto |                        (** goal: Globalenvs.Genv.find_funct *)
+         reflexivity] |                               (** goal: type_of_fundef *)
+       idtac]
+  (** forward_returnstate *)
+  | |- Smallstep.plus _ _ (Returnstate _ _ _) _ _ =>
+      eapply Smallstep.plus_left'; eauto; [eapply step_returnstate | idtac]
+  (** forward_skip_seq *)
+  | |- Smallstep.plus _ _ (State _ Sskip (Kseq _ _) _ _ _) _ _ =>
+      eapply Smallstep.plus_left'; eauto; [eapply step_skip_seq | idtac]
+  (** forward_if *)
+  | |- Smallstep.plus _ _ (State _ (Sifthenelse _ _ _) _ _ _ _) _ _ =>
+    eapply Smallstep.plus_left'; eauto;
+    [eapply step_ifthenelse;
+      [eapply eval_Etempvar; rewrite Maps.PTree.gss; reflexivity |
+       reflexivity] |
+     idtac]
+  (** forward_return_some *)
+  | |- Smallstep.plus _ _ (State _ (Sreturn _) _ _ _ _) _ _ =>
+    eapply Smallstep.plus_left'; eauto;
+      [eapply step_return_1;
+        [eapply eval_Econst_long |
+         reflexivity |
+         reflexivity] | idtac]
+ end.
 
 Definition int64_correct (x:int64_t) (v: val) (m: Memory.Mem.mem) :=
   Vlong x = v.
@@ -48,22 +121,60 @@ Section Upd_pc.
 
   Lemma correct_function3_upd_pc : correct_function3 p args res f fn modifies match_mem match_arg_list match_res.
   Proof.
-    eapply correct_function_from_body.
-    - simpl; unfold Coqlib.list_disjoint. simpl; intuition (subst; discriminate).
-    - eapply list_no_repet_dec with (eq_dec := Pos.eq_dec); reflexivity.
-    - simpl; eapply list_no_repet_dec with (eq_dec := Pos.eq_dec); reflexivity.
-    - reflexivity.
-    - reflexivity.
-    - reflexivity.
-    - intros.
-      unfold args in *.
-      car_cdr.
-      unfold app, list_rel_arg.
-      simpl.
-      unfold correct_body.
-      repeat intro.
-      unfold pre in *.
-      do 3 eexists.
+    eapply correct_function_from_body;
+    [ simpl; unfold Coqlib.list_disjoint; simpl; intuition (subst; discriminate) |
+      eapply list_no_repet_dec with (eq_dec := Pos.eq_dec); reflexivity |                             
+      simpl; eapply list_no_repet_dec with (eq_dec := Pos.eq_dec); reflexivity |
+      reflexivity |
+      reflexivity |
+      reflexivity |
+      idtac
+    ].
+    intros.
+    unfold args in *.
+    car_cdr.
+    unfold list_rel_arg.
+    simpl.
+    unfold correct_body.
+    repeat intro.
+    unfold pre in *.
+    do 3 eexists.
+    repeat split; simpl; unfold step2.
+    - 
+      apply Smallstep.plus_star.
+      repeat forward_plus.
+      eapply Smallstep.plus_left'; eauto. (*
+      repeat econstructor; eauto.
+      unfold match_elt in H.
+      unfold match_temp_env in H0.*)
+      simpl in H.
+      eapply step_assign.
+      + eapply eval_Efield_struct.
+        * eapply eval_Elvalue.
+          **
+            eapply eval_Ederef.
+            eapply eval_Etempvar. (**r mark here *)
+            unfold match_temp_env in H0.
+            unfold match_elt in H0, H.
+            rewrite Forall_forall in H0.
+            assert (Hinlist: In (_st, Clightdefs.tptr (Ctypes.Tstruct _bpf_state Ctypes.noattr), match_mem st)
+         [(_st, Clightdefs.tptr (Ctypes.Tstruct _bpf_state Ctypes.noattr), match_mem st);
+          (_pc, Clightdefs.tulong, int64_correct c)]). {
+              unfold In.
+              left; reflexivity.
+            }
+            apply H0 in Hinlist.
+            unfold fst in Hinlist.
+            destruct (Maps.PTree.get _st le) eqn: Hst_get in Hinlist.
+            destruct Hinlist as (Hctype & Hcased).
+            simpl in Hctype.
+            unfold match_mem in Hctype.
+            apply MakeMatch in Hctype.
+            unfold match_meminj_state in Hctype.
+            destruct H0 as (Hinlist).
+      econstructor; eauto.
+      
+      econstructor; eauto.
       ...
       repeat intro.
      reflexivity. repeat econstructor; eauto.
