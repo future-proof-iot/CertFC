fa3aa7a03bd66bbb162bd9dd23681a0ccb9cf48d
syuan <shenghao.yuan@inria.fr>
2021-12-08 11:21:15 +0100

    removing useless st and m

--- codeTest/CorrectRel.v
+++ codeTest/CorrectRel.v
@@ -1,4 +1,4 @@
-From dx.tests Require Import DxIntegers DxValues DxMemRegion DxRegs DxState DxMonad DxInstructions.
+From dx.tests Require Import DxIntegers DxValues DxMemRegion DxRegs DxState DxMonad DxFlag DxInstructions.
 From Coq Require Import List Lia.
 From compcert Require Import Integers Values Clight Memory AST.
 Import ListNotations.
@@ -6,14 +6,14 @@ Require Import ZArith.
 
 From bpf.proof Require Import Clightlogic MatchState.
 
-Definition int64_correct (x:int64_t) (v: val) (stm:stateM) (m: Memory.Mem.mem) :=
+Definition int64_correct (x:int64_t) (v: val) :=
   Vlong x = v.
 
 Definition val64_correct (x:val64_t) (v: val) :=
   x = v /\ exists vl, x = Vlong vl.
 
 
-Definition reg_correct (r: reg) (v: val) (stm:stateM) (m: Memory.Mem.mem) :=
+Definition reg_correct (r: reg) (v: val) :=
   (*complu_lt_32 v (Vint (Int.repr 11)) = true /\ (**r ensured by verifier *) *)
     v = Vint (Int.repr (id_of_reg r)).
 
@@ -27,3 +27,6 @@ Definition match_chunk (x : memory_chunk) (b: val) :=
           | Mint64 => Integers.Int.repr 8
           | _ => Integers.Int.repr 0
           end).
+
+Definition flag_correct (f: bpf_flag) (v: val) :=
+  v = Vint (int_of_flag f).
